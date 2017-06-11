41c5e66c4c329f9f394d47f24e105056ee1662f2
David CARLIER <devnexen@Davids-MacBook-Pro.local>
2017-06-11 19:18:27 +0100

    Find components via find_package() cmake modules

---
+++ cmake/Modules/FindApache.cmake
@@ -0,0 +1,10 @@
+find_path(APACHE_INC
+	  NAMES httpd.h
+	  HINTS
+	      /usr/include/apache2
+	      /usr/include
+	      /usr/local/include/apache2
+	      /usr/local/include/apache22
+	      /usr/local/include/apache24)
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(APACHE DEFAULT_MSG APACHE_INC)
